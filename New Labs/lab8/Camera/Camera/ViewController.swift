13ddfd851fc90afad490a4ba7f1da02cf5229b2b
Anokhi Kastia <anokhikastia@berkeley.edu>
2019-08-26 20:08:53 -0400

    Lab 8

--- New Labs/lab8/Camera/Camera/ViewController.swift
+++ New Labs/lab8/Camera/Camera/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate {
     // You may have to implement more protocols!
     
     @IBOutlet weak var imageView: UIImageView!
@@ -20,10 +21,25 @@ class ViewController: UIViewController {
     
     @IBAction func photoLibraryButtonPressed(_ sender: UIButton) {
         // Your code here
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
     }
     
     @IBAction func cameraButtonPressed(_ sender: UIButton) {
         // Your code here
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            return
+        }
     }
     
 }
